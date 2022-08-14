76ec973e4c6ee19a2e2279d31f59fb040cb6e876
Huang-Tsu <huang0966714@gmail.com>
2022-08-15 00:12:11 +0800

    Improve shortest routes 1 參考柏仲的code之後，改進了我的code 柏仲的這份code在流程上比我更優秀，好厲害。->直接把第1個點也丟進去dikjstra處理，不用特別initialize 用pair<long, long>的方法存adj_weight也挺簡潔的 pair 的.first, .second可讀性很糟，用一個變數暫存他們的值再使用會比較清楚

--- graph/shortest_routes_1.cpp
+++ graph/shortest_routes_1.cpp
@@ -1,83 +1,54 @@
 #include <iostream>
-#include <climits>
+#include <vector>
 #include <queue>
-#include <map>
+#include <cstring>
+#include <climits>
 using namespace std;
+typedef long long ll;
+#define pll pair<ll, ll>
 
-class Vertix{
- public:
-  char visited;
-  long long dis;
-  map<long, long> adj_list;
-};
-
-Vertix g_vertix[100010];
 struct comp{
-  bool operator()(long long a, long long b) {
-    return g_vertix[a].dis >= g_vertix[b].dis;
+  bool operator ()(pll a, pll b){
+    return a.second > b.second; //smaller one goes first(min heap)
   }
 };
 int main(){
-  int n, m;
-  int vertix_next, vertix_now;
-  int a, b, cost;
-  long long test_cost;
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n, m, a, b, c, now_idx, adj_idx;
+  vector<pll> weight_list[100001];
+  ll dis[100001], temp_weight, now_weight, adj_weight;
+  pll vertix_now;
+  priority_queue<pll, vector<pll>, comp> pq;
+  
   cin >> n >> m;
-  priority_queue<long long, vector<long long>, comp> prior_q;
-
-  //initialize
-  for(int i=1; i<n+1; i++){
-    g_vertix[i].visited = 0;
-    g_vertix[i].dis = LLONG_MAX;
-  }
-
   for(int i=0; i<m; i++){
-    cin >> a >> b >> cost;
-    if(a != b){
-      if(g_vertix[a].adj_list.find(b)==g_vertix[a].adj_list.end() || cost < g_vertix[a].adj_list[b]){  //not exist or the input one is smaller than the exist one
-        g_vertix[a].adj_list[b] = cost;
-      }
-    }
+    cin >> a >> b >> c; 
+    weight_list[a].push_back({b, c});  //{end, cost}
   }
-
-  g_vertix[1].dis = 0;
-  g_vertix[1].visited = 2;
-  for(const auto &next_pair : g_vertix[1].adj_list){
-    vertix_next = next_pair.first;
-    g_vertix[vertix_next].dis = next_pair.second;
-    g_vertix[vertix_next].visited = 1;
-    prior_q.push(vertix_next);
+  //initialize
+  for(int i=1; i<n+1; i++){//TODO seek for a better way (memset?)
+    dis[i] = LLONG_MAX;  
   }
-
-  while(!prior_q.empty()){
-      //cout << "prior_q.size():" << prior_q.size() << '\n';
-    vertix_now = prior_q.top(), prior_q.pop();
-    if(g_vertix[vertix_now].visited == 2){//vertix exsit
+  //start dijkstra 
+  dis[1] = 0;
+  pq.push({1, 0});
+  while(!pq.empty()){
+    vertix_now = pq.top(), pq.pop();
+    now_idx = vertix_now.first, now_weight = vertix_now.second;
+    if(now_weight > dis[now_idx]) //have been visited by shorter route
       continue;
-    }
-      //cout << "vertix_idx:" << vertix_now << '\n';
-    g_vertix[vertix_now].visited = 2;
-    //add adj_edge to priority_queue
-    for(const auto &pair_next : g_vertix[vertix_now].adj_list){
-      vertix_next = pair_next.first;
-      test_cost = g_vertix[vertix_now].dis+pair_next.second;
-      if(g_vertix[vertix_next].visited != 2){
-        if(g_vertix[vertix_next].visited == 0){
-          g_vertix[vertix_next].visited = 1;
-          g_vertix[vertix_next].dis = test_cost;
-          prior_q.push(vertix_next);
-        }
-        else if(test_cost < g_vertix[vertix_next].dis){
-          g_vertix[vertix_next].dis = test_cost;
-          prior_q.push(vertix_next);
-          //prior_q.push(vertix_next);
-        }
+    for(const auto &adj_vertix : weight_list[now_idx]){  //traverse all the neightbours of vertix_now
+      adj_idx = adj_vertix.first, adj_weight = adj_vertix.second;
+      temp_weight = now_weight + adj_weight;
+      if(temp_weight < dis[adj_idx]){
+        dis[adj_idx] = temp_weight;
+        pq.push({adj_idx, dis[adj_idx]});
       }
     }
   }
+
   for(int i=1; i<n+1; i++){
-    cout << g_vertix[i].dis << ' ';
+    cout << dis[i] << ' ';
   }
 
-  return 0; 
 }
