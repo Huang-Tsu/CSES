b39edbeb352484c66a1b6e614b6d4f8a1dace15b
root <root@DESKTOP-8NVRF2Q>
2023-05-27 16:58:37 +0800

    update sub array sums II

--- sort_search/subarray_sums_II.cpp
+++ sort_search/subarray_sums_II.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long real_x(long long x, int min_value, int input_cnt){
-  return x + min_value*input_cnt;
-}
 int main(){
   long long n, x;
-  long long answer = 0;
-  cin >> n >> x;
   long long pre = 0;
-  long long input;
   long long need;
+  long long input;
+  long long answer = 0;
   unordered_map <long long, int> pre_cnt;
-  pre_cnt[pre] ++;  //***important*** zero can be possible
+  pre_cnt[pre] ++;  //***important** zero is part of prefix sum
+  cin >> n >> x;
   for(int i=0; i<n; i++){
-    //pre[R] - pre[L-1] == x
+    //pre[R] - pre[L-1] = x;
     //pre[L-1] == pre[R] - x;
     cin >> input;
     pre += input;
     need = pre - x;
-    answer += pre_cnt[need]; 
+    answer += pre_cnt[need];
     pre_cnt[pre] ++;
   }
+  cout << answer;
 
-  cout << answer << '\n';
 }
